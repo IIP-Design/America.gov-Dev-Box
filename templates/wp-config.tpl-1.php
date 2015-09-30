da58667fca412530d18d65436c64a41eb02a042f
Terri Regan <reganta@america.gov>
2015-09-30 06:43:27 -0400

    Testing...

--- templates/wp-config.tpl-1.php
+++ templates/wp-config.tpl-1.php
@@ -1,31 +1,18 @@
 <?php
-/**
- * The base configurations of the WordPress.
- *
- * This file has the following configurations: MySQL settings, Table Prefix,
- * Secret Keys, and ABSPATH. You can find more information by visiting
- * {@link http://codex.wordpress.org/Editing_wp-config.php Editing wp-config.php}
- * Codex page. You can get the MySQL settings from your web host.
- *
- * This file is used by the wp-config.php creation script during the
- * installation. You don't have to use the web site, you can just copy this file
- * to "wp-config.php" and fill in the values.
- *
- * @package WordPress
- */
 
-// ** MySQL settings - You can get this info from your web host ** //
+
+// ** MySQL settings ** //
 /** The name of the database for WordPress */
-define('DB_NAME', getenv('OPENSHIFT_APP_NAME'));
+define('DB_NAME', 'wordpress');
 
 /** MySQL database username */
-define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
+define('DB_USER', 'wordpress');
 
 /** MySQL database password */
-define('DB_PASSWORD', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
+define('DB_PASSWORD', 'wordpress');
 
 /** MySQL hostname */
-define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST') . ':' . getenv('OPENSHIFT_MYSQL_DB_PORT'));
+define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
@@ -33,56 +20,28 @@ define('DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
-/**#@+
- * Authentication Unique Keys and Salts.
- *
- * Change these to different unique phrases!
- * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
- * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
- *
- * @since 2.6.0
- */
-define('AUTH_KEY',         getenv('AUTH_KEY'));
-define('SECURE_AUTH_KEY',  getenv('SECURE_AUTH_KEY'));
-define('LOGGED_IN_KEY',    getenv('LOGGED_IN_KEY'));
-define('NONCE_KEY',        getenv('NONCE_KEY'));
-define('AUTH_SALT',        getenv('AUTH_SALT'));
-define('SECURE_AUTH_SALT', getenv('SECURE_AUTH_SALT'));
-define('LOGGED_IN_SALT',   getenv('LOGGED_IN_SALT'));
-define('NONCE_SALT',       getenv('NONCE_SALT'));
-
-/**#@-*/
-
-/**
- * WordPress Database Table prefix.
- *
- * You can have multiple installations in one database if you give each a unique
- * prefix. Only numbers, letters, and underscores please!
- */
-$table_prefix  = 'wp_';
+define('AUTH_KEY',         'UWm+v<K4H0W/P=T!C3M_L5jcElpx*2k3R=pA/6${dC#+YaCWi/6chy0|XC7Mwb,o');
+define('SECURE_AUTH_KEY',  'ovNeXWDG>DMbUR#f7Zwu }b@f-w.ky>%<wByKH%+|LV=e~MIom<k`-<x].[lA}Tb');
+define('LOGGED_IN_KEY',    '?H8OFW+B=HF>Ob~hJ[v.dZ,[TLtsw+oPKie~o~Vm-?u)e):e%tlhm97%_4%|P/D#');
+define('NONCE_KEY',        'T%jD>e<aqt`-/>%gyvZ*[Q2NLFG8;K;+*XonE]hhD}:I:x+S?`=U%d+c1Y9VRRvq');
+define('AUTH_SALT',        '@eXz,m&-{yZ3%=_e*0c82~Ap>Wd(2m3&N3[$3<4*R3&E/aGRkinN>{dj<D?uKs7p');
+define('SECURE_AUTH_SALT', '&+#7MWf|0YD[O,NmEav]|}/-<u/wB#$YHZe#9|$XJmv=m2AP%ym/8b9sy7,YB!,9');
+define('LOGGED_IN_SALT',   '4+3;ZLjsT1(yOS;%RMOa<k]pBpoV8c5*%0v%J*w%SPuI7`/sF`D95|)4u+v4G%lq');
+define('NONCE_SALT',       '&j%t|3t+B#5wv-O%qfj2)=?a!M8fse$+mq8FKyrSLFX9~Acgb-GL;JJC)yYyM2Y}');
 
 
-// AWS site
-if (getenv('AWS_ENV') == 'STARBUCK') {
-    define('AWS_ENV', 'STARBUCK');
-} else {
-    define('AWS_ENV', 'other');
-}
+$table_prefix = 'wp_';
 
-/**
- * For developers: WordPress debugging mode.
- *
- * Change this to true to enable the display of notices during development.
- * It is strongly recommended that plugin and theme developers use WP_DEBUG
- * in their development environments.
- */
-if (getenv('DEV') == 'True') {
-    define('WP_DEBUG', true);
-    define('WP_DEBUG_LOG', true);
-} else {
-    define('WP_DEBUG', false);
-}
 
+define( 'WP_HOME', 'http://america.dev' );
+define( 'WP_SITEURL', 'http://america.dev/wp' );
+define( 'JETPACK_DEV_DEBUG', true );
+define( 'WP_DEBUG', true );
+define( 'FORCE_SSL_ADMIN', true );
+define( 'SAVEQUERIES', false );
+
+
+// Tells Wordpress to look for the wp-content directory in a non-standard location
 define('WP_CONTENT_DIR', __DIR__ . '/wp-content');
 
 if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) and $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
@@ -95,7 +54,6 @@ if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) and $_SERVER['HTTP_X_FORWARDED_PRO
     define('WP_SITEURL', 'http://' . $_SERVER['SERVER_NAME'] . '/wp');
     define('WP_HOME', 'http://' . $_SERVER['SERVER_NAME']);
 }
-
 
 /* That's all, stop editing! Happy blogging. */
 
